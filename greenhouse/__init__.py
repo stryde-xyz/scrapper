26ac7b408da37a379f95e2db18a752d20be37a97
Gore, Sumit <sumit.gore@gt-arc.com>
2025-09-21 19:49:42 +0200

    fix pandas header

--- greenhouse/__init__.py
+++ greenhouse/__init__.py
@@ -2,7 +2,7 @@ from greenhouse.list import GreenHouseList
 import pandas as pd
 
 def load_csv(file_path):
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path, header=None)
     df.columns = ["company_name"]
     company_name = [data for data in df["company_name"]]
     return company_name
