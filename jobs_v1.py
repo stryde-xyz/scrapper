08958801076ed8d905a78742b3068d5ad06a0370
Gore, Sumit <sumit.gore@gt-arc.com>
2025-09-21 18:55:38 +0200

    handle errors

--- jobs_v1.py
+++ jobs_v1.py
@@ -32,19 +32,22 @@ try:
             existin_job = session.query(Job).filter_by(id=job.id).first()
             # print(existin_job)
             if existin_job is None:
-                html_content = html.unescape(str(job.content))
-                published_at = parser.parse(str(job.published_at))
-                if job.published_at is Null:
-                    published_at = parser.parse(str(job.updated_at))
-                updated_at = parser.parse(str(job.updated_at))
+                try:
+                    html_content = html.unescape(str(job.content))
+                    published_at = parser.parse(str(job.published_at))
+                    if job.published_at is Null:
+                        published_at = parser.parse(str(job.updated_at))
+                    updated_at = parser.parse(str(job.updated_at))
 
-                _job_to_add = Job(id=job.id, name=job.name, internal_job_id=job.internal_job_id, published_at=published_at,
-                    updated_at=updated_at,  apply_url = job.apply_url, content = html_content, location = job.location,
-                    company = job.company)
+                    _job_to_add = Job(id=job.id, name=job.name, internal_job_id=job.internal_job_id, published_at=published_at,
+                        updated_at=updated_at,  apply_url = job.apply_url, content = html_content, location = job.location,
+                        company = job.company)
 
-                session.add(_job_to_add)
-                print(f"added {_job_to_add.id}")
-                session.commit()
+                    session.add(_job_to_add)
+                    print(f"added {_job_to_add.id}")
+                    session.commit()
+                except Exception as e:
+                    print(e)
 
 except Exception as e:
     print(e)
